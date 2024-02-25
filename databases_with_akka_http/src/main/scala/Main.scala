8c9430dbd677820996d9022cc3baa1a5bdaef516
Amit Singh <amit8singh008@gmail.com>
2024-02-25 19:32:40 +0530

    sample akka http program

--- databases_with_akka_http/src/main/scala/Main.scala
+++ databases_with_akka_http/src/main/scala/Main.scala
@@ -1,5 +1,33 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Accessing Database with Akka-http API")
-  }
+import akka.actor.typed.javadsl.Behaviors
+import akka.actor.typed.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server.Directives.{complete, path}
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
+object Main extends App {
+  println("Accessing Database with Akka-http API")
+
+  implicit val system = ActorSystem(Behaviors.empty, "my-system")
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext: ExecutionContextExecutor = system.executionContext
+
+  val route1 =
+    path("hello") {
+      Directives.get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+  val bindingFuture = Http().newServerAt("localhost", 8080)
+    .bind(route1)
+
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) //trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
 }
