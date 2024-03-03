342e51728c143379934f429145f5010d06bdf678
Amit Singh <amit8singh008@gmail.com>
2024-03-03 20:21:35 +0530

    Using akkaServer class as main which keep the application running works with slick to execute the queries

--- databases_with_akka_http/src/main/scala/akkaServer.scala
+++ databases_with_akka_http/src/main/scala/akkaServer.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.Directives.{complete, path}
+import com.amit.db.DbConnection
 //import com.amit.db.DbConnection.setupFuture
 import com.typesafe.scalalogging.Logger
 
@@ -13,23 +14,25 @@ import scala.io.StdIn
 object akkaServer extends App {
   val logger = Logger(getClass.getName)
   logger.info("Accessing Database with Akka-http API")
-//  implicit val system = ActorSystem(Behaviors.empty, "my-system")
-//  // needed for the future flatMap/onComplete in the end
-//  implicit val executionContext: ExecutionContextExecutor = system.executionContext
-//
-//  val route1 =
-//    path("hello") {
-//      Directives.get {
-//        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-//      }
-//    }
-//
-//  val bindingFuture = Http().newServerAt("localhost", 8080)
-//    .bind(route1)
-//
-//  StdIn.readLine() // let it run until user presses return
-//  bindingFuture
-//    .flatMap(_.unbind()) //trigger unbinding from the port
-//    .onComplete(_ => system.terminate()) // and shutdown when done
+  implicit val system = ActorSystem(Behaviors.empty, "my-system")
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext: ExecutionContextExecutor = system.executionContext
+
+  val route1 =
+    path("hello") {
+      Directives.get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+  DbConnection.getting_result()
+  val bindingFuture = Http().newServerAt("localhost", 8080)
+    .bind(route1)
+
+
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) //trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 
 }
--- databases_with_akka_http/src/main/scala/com/amit/db/DbConnection.scala
+++ databases_with_akka_http/src/main/scala/com/amit/db/DbConnection.scala
@@ -10,7 +10,7 @@ import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
-object DbConnection extends App {
+object DbConnection {
 
   val logger = Logger(getClass.getName)
   val db = Database.forConfig("mySqlDb")
@@ -43,59 +43,61 @@ object DbConnection extends App {
   }
   val coffees = TableQuery[Coffees]
 //
-val setup = DBIO.seq(
-  // Create the tables, including primary and foreign keys
-  (suppliers.schema ++ coffees.schema).create,
-
-  // Insert some suppliers
-  suppliers += (101, "Acme, Inc.",      "99 Market Street", "Groundsville", "CA", "95199"),
-  suppliers += ( 49, "Superior Coffee", "1 Party Place",    "Mendocino",    "CA", "95460"),
-  suppliers += (150, "The High Ground", "100 Coffee Lane",  "Meadows",      "CA", "93966"),
-  // Equivalent SQL code:
-  // insert into SUPPLIERS(SUP_ID, SUP_NAME, STREET, CITY, STATE, ZIP) values (?,?,?,?,?,?)
-
-  // Insert some coffees (using JDBC's batch insert feature, if supported by the DB)
-  coffees ++= Seq(
-    ("Colombian",         101, 7.99, 0, 0),
-    ("French_Roast",       49, 8.99, 0, 0),
-    ("Espresso",          150, 9.99, 0, 0),
-    ("Colombian_Decaf",   101, 8.99, 0, 0),
-    ("French_Roast_Decaf", 49, 9.99, 0, 0)
-  )
-  // Equivalent SQL code:
-  // insert into COFFEES(COF_NAME, SUP_ID, PRICE, SALES, TOTAL) values (?,?,?,?,?)
-)
-
-  db.run(setup)
-
-//  case class Movie(id: Int, name: String, year: Int)
-//  class Movies(tag: Tag) extends Table[(Int, String, Int)](tag, Option[String]("test01"), "movies") {
-//    def id = column[Int]("id")
-//    def name = column[String]("name")
-//    def year = column[Int]("year")
-//    def * = (id, name, year)
-//  }
+//val setup = DBIO.seq(
+//  // Create the tables, including primary and foreign keys
+//  (suppliers.schema ++ coffees.schema).create,
 //
-//  val movies = TableQuery[Movies]
+//  // Insert some suppliers
+//  suppliers += (101, "Acme, Inc.",      "99 Market Street", "Groundsville", "CA", "95199"),
+//  suppliers += ( 49, "Superior Coffee", "1 Party Place",    "Mendocino",    "CA", "95460"),
+//  suppliers += (150, "The High Ground", "100 Coffee Lane",  "Meadows",      "CA", "93966"),
+//  // Equivalent SQL code:
+//  // insert into SUPPLIERS(SUP_ID, SUP_NAME, STREET, CITY, STATE, ZIP) values (?,?,?,?,?,?)
+//
+//  // Insert some coffees (using JDBC's batch insert feature, if supported by the DB)
+//  coffees ++= Seq(
+//    ("Colombian",         101, 7.99, 0, 0),
+//    ("French_Roast",       49, 8.99, 0, 0),
+//    ("Espresso",          150, 9.99, 0, 0),
+//    ("Colombian_Decaf",   101, 8.99, 0, 0),
+//    ("French_Roast_Decaf", 49, 9.99, 0, 0)
+//  )
+//  // Equivalent SQL code:
+//  // insert into COFFEES(COF_NAME, SUP_ID, PRICE, SALES, TOTAL) values (?,?,?,?,?)
+//)
+//
+//  db.run(setup)
+
+  case class Movie(id: Int, name: String, year: Int)
+  class Movies(tag: Tag) extends Table[(Int, String, Int)](tag, Option[String]("test01"), "movies") {
+    def id = column[Int]("id")
+    def name = column[String]("name")
+    def year = column[Int]("year")
+    def * = (id, name, year)
+  }
+
+  val movies = TableQuery[Movies]
 
   /*
   * onComplete attaches a callback function to the Future.
   *  This function will be invoked once the Future completes,
   *  either successfully (Success) or with an error (Failure).
   * */
-//  db.run(movies.result).onComplete {
-//    case Success(results) =>
-//      println("Query completed, processing results.")
-//      results.foreach(println)
-//    case Failure(exception) =>
-//      println(s"Error executing query: $exception")
-//  }
 
+  def getting_result(): Unit ={
+    db.run(movies.result).onComplete {
+      case Success(results) =>
+        println("Query completed, processing results.")
+        results.foreach(println)
+      case Failure(exception) =>
+        println(s"Error executing query: $exception")
+    }
+  }
 
 //  val results = Await.result(db.run(movies.result), Duration.Inf)
 //  results.foreach(println)
   logger.info("This file is completed")
-  Thread.sleep(5000)
+//  Thread.sleep(5000)
 
 //  db.close()
 }
